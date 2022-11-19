377a4e9931f72bc95a7da8fa867c02ab5d091618
Samy69000 <113507198+Samy69000@users.noreply.github.com>
2022-11-19 19:30:12 +0100

    first commit

---
+++ ex01/bonjour.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Bonjour, Quel est votre prénom ?"
+read var1
+echo bonjour, $var1 !
+ 
---
+++ ex04/print_groups.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "niters,adm,cdrom,sudo,dip,plugdev,lpadmin,lxd,sambashare"
+
